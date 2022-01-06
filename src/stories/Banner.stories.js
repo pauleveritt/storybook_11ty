d74a3aa8850385805d10b7d3b9c7886a127082ec
Paul Everitt <pauleveritt@me.com>
2022-01-06 16:20:45 -0500

    Nunjucks macro rendering as a story.

--- src/stories/Banner.stories.js
+++ src/stories/Banner.stories.js
@@ -1,41 +1,14 @@
-import { action } from '@storybook/addon-actions';
-
-import Button from  './Banner.njk';
+import Banner from  './Banner.njk';
 
 export default {
-    title: 'Design System/Atoms/Button',
-    argTypes: {
-        label: { control: 'text' }
-    }
+    title: 'Example/Banner'
 };
 
-const Template = ({ onClick, label }) => {
-    return Button({ label });
+const Template = ({ label }) => {
+    return Banner({ label });
 };
 
-export const Text = Template.bind({});
-Text.args = {
-    label: 'Button',
-    onClick: action('onClick')
+export const Primary = Template.bind({});
+Primary.args = {
+    label: 'Hello Banner',
 };
-
-// // More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
-// export default {
-//     title: 'Example/Banner',
-//     // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
-//     argTypes: {},
-// };
-//
-// // More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
-// const Template = ({label, ...args}) => {
-//     // You can either use a function to create DOM elements or use a plain html string!
-//     // return `<div>${label}</div>`;
-//     return createBanner({label, ...args});
-// };
-//
-// export const Primary = Template.bind({});
-// // More on args: https://storybook.js.org/docs/html/writing-stories/args
-// Primary.args = {
-//     primary: true,
-//     label: 'Button',
-// };
