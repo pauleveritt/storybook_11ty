f53649b9bd4fb9f8db2270e9a100a11cf00322d8
Paul Everitt <pauleveritt@me.com>
2022-01-06 16:16:16 -0500

    Switch to webpack5.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
@@ -7,5 +10,24 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/html"
+  "framework": "@storybook/html",
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.njk$/,
+      use: [
+        {
+          loader: 'simple-nunjucks-loader',
+          options: {}
+        }
+      ]
+    });
+
+    // Return the altered config
+    return config;
+  }
 }
--- src/stories/Banner.stories.js
+++ src/stories/Banner.stories.js
@@ -1,22 +1,41 @@
-import {createBanner} from './Banner';
+import { action } from '@storybook/addon-actions';
 
-// More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
+import Button from  './Banner.njk';
+
 export default {
-    title: 'Example/Banner',
-    // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
-    argTypes: {},
+    title: 'Design System/Atoms/Button',
+    argTypes: {
+        label: { control: 'text' }
+    }
 };
 
-// More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
-const Template = ({label, ...args}) => {
-    // You can either use a function to create DOM elements or use a plain html string!
-    // return `<div>${label}</div>`;
-    return createBanner({label, ...args});
+const Template = ({ onClick, label }) => {
+    return Button({ label });
 };
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/html/writing-stories/args
-Primary.args = {
-    primary: true,
+export const Text = Template.bind({});
+Text.args = {
     label: 'Button',
+    onClick: action('onClick')
 };
+
+// // More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
+// export default {
+//     title: 'Example/Banner',
+//     // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
+//     argTypes: {},
+// };
+//
+// // More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
+// const Template = ({label, ...args}) => {
+//     // You can either use a function to create DOM elements or use a plain html string!
+//     // return `<div>${label}</div>`;
+//     return createBanner({label, ...args});
+// };
+//
+// export const Primary = Template.bind({});
+// // More on args: https://storybook.js.org/docs/html/writing-stories/args
+// Primary.args = {
+//     primary: true,
+//     label: 'Button',
+// };
