1c5d5c2b2b91bb904ee2e96236ffdfe01126e615
Paul Everitt <pauleveritt@me.com>
2022-01-06 15:39:41 -0500

    Nunjucks with Webpack done.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.njk$/,
+                use: [
+                    {
+                        loader: 'simple-nunjucks-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
     mode: 'development',
     entry: './src/index.js',
     output: {
+        path: path.resolve(__dirname, './dist'),
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'src/template.njk',
+            templateParameters: {
+                username: 'Joe'
+            }
+        })
+    ]
 };
