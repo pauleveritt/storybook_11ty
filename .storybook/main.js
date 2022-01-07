411e7d1d302b3a29072b79a26700154c7691319d
Paul Everitt <pauleveritt@me.com>
2022-01-07 17:14:45 -0500

    Let's declare failure.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,41 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/html",
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.njk$/,
-      use: [
-        {
-          loader: 'simple-nunjucks-loader',
-          options: {}
-        }
-      ]
-    });
-
-    // Return the altered config
-    return config;
-  }
+    core: {
+        builder: 'webpack5',
+    },
+    "stories": [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials"
+    ],
+    "framework": "@storybook/html",
+    // webpackFinal: async (config, {configType}) => {
+    //     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    //     // You can change the configuration based on that.
+    //     // 'PRODUCTION' is used when building the static version of storybook.
+    //
+    //     // Make whatever fine-grained changes you need
+    //     config.target = "node";
+    //     config.resolve.fallback["perf_hooks"] = false;
+    //     config.resolve.fallback["fs"] = require.resolve('fs');
+    //     // config.browser = {fs: require.resolve('fs')}
+    //     config.performance.maxEntrypointSize = 2000000;
+    //     config.performance.maxAssetSize = 2000000;
+    //     // config.browser = { "fs": false, "path": false, "os": false, "net": false, "tls": false };
+    //
+    //     config.module.rules.push({
+    //         test: /\.njk$/,
+    //         use: [
+    //             {
+    //                 loader: 'simple-nunjucks-loader',
+    //                 options: {}
+    //             }
+    //         ]
+    //     });
+    //
+    //     // Return the altered config
+    //     return config;
+    // }
 }
--- src/stories/Banner.stories.js
+++ src/stories/Banner.stories.js
@@ -1,11 +1,15 @@
 import Banner from '../_includes/Banner.njk';
+const TemplateRender = require("@11ty/eleventy/src/TemplateRender");
+
+// import TemplateConfig from "@11ty/eleventy/src/TemplateConfig";
 
 export default {
     title: 'Example/Banner'
 };
 
 const Template = ({ label }) => {
-    return Banner({ label });
+    const banner = Banner({label});
+    return banner;
 };
 
 export const Primary = Template.bind({});
